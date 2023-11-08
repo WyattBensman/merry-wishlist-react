28c1838b9bb6799c094ae0be7db457f6dca31f13
WyattBensman <wyattbensman5@gmail.com>
2023-11-08 08:28:22 -0500

    Completed Queries on the Client side

---
+++ client/src/utils/queries.js
@@ -0,0 +1,63 @@
+import { gql } from "@apollo/client";
+
+export const GET_USER = gql`
+  query GetUser($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      fName
+      lName
+      email
+      lists {
+        _id
+        title
+        listItems {
+          _id
+          itemName
+          itemPrice
+          itemSize
+          itemUrl
+        }
+      }
+      savedStores {
+        _id
+        name
+        url
+        image
+      }
+    }
+  }
+`;
+
+export const GET_LIST = gql`
+  query GetList($listId: ID!) {
+    list(listId: $listId) {
+      _id
+      userId {
+        _id
+        fName
+        lName
+        email
+      }
+      active
+      title
+      listItems {
+        _id
+        itemName
+        itemPrice
+        itemSize
+        itemUrl
+      }
+    }
+  }
+`;
+
+export const GET_STORES = gql`
+  query GetStores {
+    stores {
+      _id
+      name
+      url
+      image
+    }
+  }
+`;
